77be91f7edb5f947e8ad38d1bca86661e1714aaa
Erik Fakin <e.fakin3@gmail.com>
2024-10-07 09:37:59 +0200

    Funzioni

--- funzioni.py
+++ funzioni.py
@@ -29,4 +29,14 @@ descrivi_persona("Massimo", 17, "3B")
 #Parametri con nome, non centra l'ordine
 descrivi_persona(eta=16, classe="3A", nome="Lara")
 
-descrivi_persona()
+
+#Dichiarazione della funzione con valori predefiniti.
+#Se non passo gli argomenti, il valore sara' uguale
+#ai defaults che ho definito nella funzione
+def calcola_volume(a=1, b=1, c=1):
+    print("Il volume del parallelepipedo",a,b,c,"e'", a*b*c)
+
+#Se non passo i parametri, il loro valore sara' uguale a 1
+#come definito della funzione
+calcola_volume()
+ 
